339096da1e445281433aa43e18484e57eb737fb0
Justin Price <mr.price@Justins-iMac.local>
2020-03-31 21:34:56 -0400

    component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,4 +1,4 @@
-// import React, {Component} from 'react';
+import React, {Component} from 'react';
 // import axios from 'axios';
 
 
@@ -153,7 +153,7 @@
 //   );
 // }
 
-import React from 'react';
+// import React from 'react';
 import axios from 'axios';
 
 class ContactForm extends Component{
