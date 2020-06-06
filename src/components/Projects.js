adb086839469d3b131098dc0186e24059586b0ae
Justin Price <mr.price@Justins-iMac.local>
2020-06-06 11:39:38 -0400

    honorable mention for python, delted redux as a category

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ const Projects = () => {
     <div>
       <h1>Projects Portfoio</h1>
       <div className="Web Projects">
-        <h2>Web Design Projects</h2>
+        <h2>Web Projects</h2>
         <div className="ui-projects">
           <h3>HTML/CSS</h3>
           {/* 1st build week project */}
@@ -18,12 +18,23 @@ const Projects = () => {
         <div className="react-projects">
           <h3>React</h3>
         </div>
-        <div classname="redux-projects">
+        {/* <div classname="redux-projects">
           <h3>Redux</h3>
-        </div>
+        </div> */}
       </div>
       <div className="Design Projects">
         <h2> Design Projects</h2>
+      </div>
+      <div className="python-skills">
+        <h3>Honorable Mention Python Github Repos:</h3>
+        <ul>
+          <li>
+            <a>
+              https://github.com/maggieprice/Sprint-Challenge--Intro-Python/tree/maggieprice
+            </a>
+            {/* This is a placeholder link, change this later */}
+          </li>
+        </ul>
       </div>
     </div>
   );
