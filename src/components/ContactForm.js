fe07ccf311794dc2db6d0878dbd0717f93609e9c
Justin Price <mr.price@Justins-iMac.local>
2020-06-06 10:51:26 -0400

    changed content wording on home page and a few CSS things for presentability while I am working

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,65 +1,75 @@
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-class ContactForm extends Component{
-  
-    handleSubmit(e){
-        e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('message').value;
-        axios({
-            method: "POST", 
-            url:"https://portfoliomail.herokuapp.com/send", 
-            data: {
-                name: name,   
-                email: email,  
-                messsage: message
-            }
-        }).then((response)=>{
-            if (response.data.msg === 'success'){
-                alert("Message Sent."); 
-                this.resetForm()
-            }else if(response.data.msg === 'fail'){
-                alert("Message failed to send.")
-            }
-        }) .catch(err=>console.log(err))
-    }
+class ContactForm extends Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    axios({
+      method: "POST",
+      url: "http://localhost:4000/send",
+      data: {
+        name: name,
+        email: email,
+        messsage: message,
+      },
+    })
+      .then((response) => {
+        if (response.data.msg === "success") {
+          alert("Message Sent.");
+          this.resetForm();
+        } else if (response.data.msg === "fail") {
+          alert("Message failed to send.");
+        }
+      })
+      .catch((err) => console.log(err));
+  }
 
-    resetForm(){
-        document.getElementById('contact-form').reset();
-    }
+  resetForm() {
+    document.getElementById("contact-form").reset();
+  }
 
-    
+  render() {
+    return (
+      <div>
+        <h1>We Should Hang Out...</h1>
+        <div className="social-media-links">
+          {/* LinkedIn, Twitter, Github,  */}
+        </div>
 
-    render(){
-        return(
-            <div>
-                <h1>We Should Hang Out...</h1>
-                    <div className="social-media-links">
-                        {/* LinkedIn, Twitter, Github,  */}
-                    </div>
-            
-            <div className="col-sm-4 offset-sm-4">
-                <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-                    <div className="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" className="form-control" id="name" />
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
-                    </div>
-                    <div className="form-group">
-                        <label for="message">Message</label>
-                        <input className="form-control" rows="5" id="message"></input>
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
+        <div className="col-sm-4 offset-sm-4">
+          <form
+            id="contact-form"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" id="name" />
             </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+              />
             </div>
-        )
-    }
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <input className="form-control" rows="5" id="message"></input>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default ContactForm; 
+export default ContactForm;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,62 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            <div className="top">
-                <div className="name">
-                    <h1>{"<"} Maggie J. Price {"/ >"}</h1>
-                    <h3>Full Stack Web Developer/Software Engineer</h3>
-                </div>
-                <img className="header-img" src="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2704&q=80" alt="laptop on table"/>
-            </div>
-            <div className="about">
-                {/* <img > */}
-                <div className="about-paragraph">
-                    <h3>About Me</h3>
-                    <p>I am a creative. Realizing this allowed me to find my passion in web development. I lovbe creating and I love problem solving. So, web development is very fullfilling for me. I am also pursuing a bachelor's of business economics degree.  </p>
-                    <h3>My Personal Mission...</h3>
-                    <p>My personal mission is to disrupt the current tech space with innovative technologies. I intend to learn and grow consistently to accomplish this.</p>
-                    {/* <a>LinkedIn</a> */}
-                </div>
-            <div className="hobbies">
-                <h3>I Love....</h3>
-                <ul>
-                    <li>Decorating</li>
-                    <li>Singing</li>
-                    <li>Playing Piano</li>
-                    <li>Listening to Podcasts, Youtube Videos, and Certain Talk Shows to Learn</li>
-                    <li>Streaming TV</li>
-                    <li>Listening to Music</li>
-                    <li>Helping Others</li>
-                    <li>Crafting</li>
-                </ul>
-            </div>
+  return (
+    <div>
+      <div className="top">
+        <div className="name">
+          <h3>Looking to hire a web developer? Meet me.</h3>
+          <h1>
+            {"<"} Maggie J. Price {"/ >"}
+          </h1>
+          <h3>Full Stack Web Developer/Software Engineer</h3>
         </div>
+        {/* <img
+          className="header-img"
+          src="https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2704&q=80"
+          alt="laptop on table"
+        /> */}
+      </div>
+      <div className="about">
+        {/* <img > */}
+        <div className="about-paragraph">
+          <h3>Who am I?</h3>
+          <p>
+            Hi. I am a creative. Realizing this allowed me to find my passion in
+            web development. I love creating and problem solving. So, web
+            development is very fulfilling for me. I am also pursuing a Bachelor
+            of Business Economics degree because... to be honest, I love
+            learning. Learning is my happy place. Learning is my down time. This
+            is just another reason why I love development! There is no shortage
+            of learning opportunities.{" "}
+          </p>
+          <div className="hobbies">
+            <h3>What do I Love?</h3>
+            <ul>
+              <li>Decorating</li>
+              <li>Singing</li>
+              <li>Playing Piano</li>
+              <li>
+                Listening to Podcasts, Youtube Videos, and Certain Talk Shows to
+                Learn
+              </li>
+              <li>Streaming TV</li>
+              <li>Listening to Music</li>
+              <li>Helping Others</li>
+              <li>Crafting</li>
+            </ul>
+          </div>
+          <h3>What is my personal mission?</h3>
+          <p>
+            My personal mission is to disrupt the current tech space with
+            innovative technologies. I intend to learn and grow consistently to
+            accomplish this. I am really facinated by the role that technology
+            plays in "changing the world" and I want to participate.
+          </p>
+        </div>
+      </div>
     </div>
-    );
-  
+  );
 }
 
 export default Home;
