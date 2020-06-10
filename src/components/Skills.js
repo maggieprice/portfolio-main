3dbffdecc2c560f712fada8fc30d352125633bb8
Justin Price <mr.price@Justins-iMac.local>
2020-06-10 12:11:10 -0400

    saving in case lose power

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,71 +1,11 @@
 import React from "react";
+import SkillsAPI from "./SkillsAPI";
 
 const Skills = () => {
   return (
     <div className="skills-container">
-      <div className="coding-skills">
-        <h1>Skills</h1>
-        <h3>Languages:</h3>
-        <ul>
-          <li> React</li>
-          React Hooks | Redux
-          <li> Javascript</li>
-          Asynchronous JavaScript
-          <li> Node js</li>
-          Express JS | SQL
-          <li> CSS</li>
-          <li> Less</li>
-          Sass
-          <li> HTML</li>
-          <li>Algorithms</li>
-          <li>Big-O Notation</li>
-          <li>Data Structures</li>
-          <li>Python</li>
-        </ul>
-        <h3>Tools:</h3>
-        <ul>
-          <li>Github</li>
-          <li>Git</li>
-          <li>Postman</li>
-          <li>Sqlite3</li>
-          <li>Figma</li>
-          <li>Trello</li>
-          <li>Codepen</li>
-          <li>CodeSandbox</li>
-          <li>VSCode</li>
-          <li>Terminal</li>
-        </ul>
-        <h3>Systems:</h3>
-        <ul>
-          <li>Mac</li>
-          <li>Windows</li>
-        </ul>
-        <h4>Exposure to... / Familiarity with... </h4>
-        <ul>
-          <li>Bootstrap</li>
-          <li>Styled Components</li>
-        </ul>
-      </div>
-      <div ClassName="other-skills">
-        <h3>Office/Business:</h3>
-        <ul>
-          <li> Correspondence</li>
-          <li> Bookeeping</li>
-          <li> Accounts Payable</li>
-          <li> Compliance</li>
-          <li> Tech Support</li>
-          <li> Accounts Receiveable/Record Keeping</li>
-          <li> Dictation</li>
-          <li> Scheduling</li>
-          <li> Banking</li>
-        </ul>
-        <h3>Tools:</h3>
-        <ul>
-          <li>Microsoft Office</li>
-          <li>Quicken</li>
-        </ul>
-      </div>
-      <div className="tech-skills">
+      <div className="tech-skills"></div>
+      <div className="design-skills">
         <h3>Honorable Mention to things I enjoy creating as a designer:</h3>
         <ul>
           <li> Invitations</li>
