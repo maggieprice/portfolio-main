2ebeae7a25f9f557d19840b592dce183e742c08e
Maggie price <maggieprice2016@gmail.com>
2020-07-14 16:12:17 -0400

    commented out skills

--- src/components/SkillsAPI.js
+++ src/components/SkillsAPI.js
@@ -1,79 +1,79 @@
-var SkillsAPI = {
-  Tech : {
-   Languages  :{
-   React1 : {Hooks : { level: "three" }, Redux : { level: "three" }},
-   Javascript : {
-       AsynchronousJavaScript : { level: "three" },
-       AlgorithmsJ : { level: "three" },
-     },
-     HTMLx : { level: "three" },
-   Nodejs : {
-       Expressjs : { level: "three" },
-       SQLx : { level: "three" },
-     },
-     CSS : { level: "three" },
-     Less : { level: "three" },
-     Sass : { level: "three" },
-   Python : {
-       BigO : { level: "three" },
-       DataStr : { level: "three" },
-       AlgorithmsP : { level: "three" },
-     },
-   },
- Libraries : {
-     Bootstrap : { level: "three" },
-     StyledComponents : { level: "three" },
-     ReactSuite : { level: "three" },
-     MaterialUI : { level: "three" },
-   },
- Tools : {
-     Github : { level: "three" },
-     Git : { level: "three" },
-     Postman : { level: "three" },
-     Sqlite3 : { level: "three" },
-     Figma : { level: "three" },
-     Trello : { level: "three" },
-     Codepen : { level: "three" },
-     CodeSandbox : { level: "three" },
-     VSCode : { level: "three" },
-     Terminal : { level: "three" },
-   },
- Systems : {Mac : { level: "three" }, Windows : { level: "three" }},
-  },
-Office : {
- Finance : {
-     Banking : { level: "three" },
-     Bookeeping : { level: "three" },
-     AccountsPR : { level: "three" },
-   },
- Clerical : {
-     Correspondence : { level: "three" },
-     Dictation : { level: "three" },
-     Scheduling : { level: "three" },
-   },
- Legal : {
-     Compliance : { level: "three" },
-     Records : { level: "three" },
-   },
- TSupport : {
-     Computers : { level: "three" },
-     Printers : { level: "three" },
-     Misc : { level: "three" },
-   },
- ToolsO : {
-   Microsoft : {
-       Word : { level: "three" },
-       Ppt : { level: "three" },
-       Excel : { level: "three" },
-       Publisher : { level: "three" },
-     },
-   Quicken : {
-       Bookeeping1 : { level: "three" },
-       Budgets : { level: "three" },
-       Reports : { level: "three" },
-     },
-   },
- },
-};
+// var SkillsAPI = {
+//   Tech : {
+//    Languages  :{
+//    React1 : {Hooks : { level: "three" }, Redux : { level: "three" }},
+//    Javascript : {
+//        AsynchronousJavaScript : { level: "three" },
+//        AlgorithmsJ : { level: "three" },
+//      },
+//      HTMLx : { level: "three" },
+//    Nodejs : {
+//        Expressjs : { level: "three" },
+//        SQLx : { level: "three" },
+//      },
+//      CSS : { level: "three" },
+//      Less : { level: "three" },
+//      Sass : { level: "three" },
+//    Python : {
+//        BigO : { level: "three" },
+//        DataStr : { level: "three" },
+//        AlgorithmsP : { level: "three" },
+//      },
+//    },
+//  Libraries : {
+//      Bootstrap : { level: "three" },
+//      StyledComponents : { level: "three" },
+//      ReactSuite : { level: "three" },
+//      MaterialUI : { level: "three" },
+//    },
+//  Tools : {
+//      Github : { level: "three" },
+//      Git : { level: "three" },
+//      Postman : { level: "three" },
+//      Sqlite3 : { level: "three" },
+//      Figma : { level: "three" },
+//      Trello : { level: "three" },
+//      Codepen : { level: "three" },
+//      CodeSandbox : { level: "three" },
+//      VSCode : { level: "three" },
+//      Terminal : { level: "three" },
+//    },
+//  Systems : {Mac : { level: "three" }, Windows : { level: "three" }},
+//   },
+// Office : {
+//  Finance : {
+//      Banking : { level: "three" },
+//      Bookeeping : { level: "three" },
+//      AccountsPR : { level: "three" },
+//    },
+//  Clerical : {
+//      Correspondence : { level: "three" },
+//      Dictation : { level: "three" },
+//      Scheduling : { level: "three" },
+//    },
+//  Legal : {
+//      Compliance : { level: "three" },
+//      Records : { level: "three" },
+//    },
+//  TSupport : {
+//      Computers : { level: "three" },
+//      Printers : { level: "three" },
+//      Misc : { level: "three" },
+//    },
+//  ToolsO : {
+//    Microsoft : {
+//        Word : { level: "three" },
+//        Ppt : { level: "three" },
+//        Excel : { level: "three" },
+//        Publisher : { level: "three" },
+//      },
+//    Quicken : {
+//        Bookeeping1 : { level: "three" },
+//        Budgets : { level: "three" },
+//        Reports : { level: "three" },
+//      },
+//    },
+//  },
+// };
 
-export default SkillsAPI;
+// export default SkillsAPI;
