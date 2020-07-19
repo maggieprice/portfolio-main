1f50013034e2754cad860306936c33ea2046e586
Maggie price <maggieprice2016@gmail.com>
2020-07-19 18:34:29 -0400

    formatted navigation properly (minus highlighted link color)

--- src/App.js
+++ src/App.js
@@ -5,47 +5,11 @@ import Home from './components/Home';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import ContactForm from './components/ContactForm';
-// import Nav from './components/Nav';
 import NavComponent from './components/NavComponent';
 
 export default function App() {
   return (
     <NavComponent/>
-    // <Router>
-    //   <div>
-    //     <ul>
-    //       <li>
-    //         <Link to="/">Home</Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/skills">Skills</Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/projects">Projects</Link>
-    //       </li>
-    //       <li>
-    //         <Link to="/contact">Contact</Link>
-    //       </li>
-    //     </ul>
-
-    //     <hr />
-
-    //     <Switch>
-    //       <Route exact path="/">
-    //         <Home />
-    //       </Route>
-    //       <Route path="/skills">
-    //         <Skills />
-    //       </Route>
-    //       <Route path="/projects">
-    //         <Projects />
-    //       </Route>
-    //       <Route path="/contact">
-    //         <ContactForm />
-    //       </Route>
-    //     </Switch>
-    //   </div>
-    // </Router>
   );
 }
 
--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -5,34 +5,32 @@ import Home from './Home';
 import Skills from './Skills';
 import Projects from './Projects';
 import ContactForm from './ContactForm';
-// import { Router } from 'react-router-dom';
 
 export default function NavComponent() {
   return (
     <Router>
-    <Nav className="navigation" tabs>
-      <NavItem>
-        <NavLink active href="/" >
+      <div className="navigation">
+    <Nav tabs>
+      <NavItem className="navigation1">
+        <NavLink className="navigation" href="/" >
           Home
         </NavLink>
-        {/* <Home/> */}
       </NavItem>
-      <NavItem>
-        <NavLink href="/skills">
+      <NavItem className="navigation1">
+        <NavLink NavLink className="navigation" href="/skills">
           Skills
         </NavLink>
-        {/* <Skills/> */}
       </NavItem>
-      <NavItem>
-        <NavLink href="/projects">Projects</NavLink>
-        {/* <Projects/> */}
+      <NavItem className="navigation1">
+        <NavLink NavLink className="navigation" href="/projects">Projects</NavLink>
       </NavItem>
-      <NavItem>
-        <NavLink href="/contact">
+      <NavItem className="navigation1">
+        <NavLink className="navigation" href="/contact">
           Contact
         </NavLink>
-        {/* <ContactForm/> */}
       </NavItem>
+      </Nav>
+      </div>
       <Switch>
           <Route exact path="/">
             <Home />
@@ -47,68 +45,6 @@ export default function NavComponent() {
             <ContactForm />
           </Route>
         </Switch>
-
-    
-    </Nav>
-    </Router>
+        </Router>
   );
 }
-// import { Nav, Dropdown } from 'rsuite';
-// import {NavLink} from 'react-router-dom'
-
-// export default function Nav() {
-//     return (
-//         <Nav>
-//             <NavLink to="/" exact={true} className={'links'}>Home</NavLink>
-//             <NavLink to="/skills"  className={'links'}>Skills</NavLink>
-//             <NavLink to="/projects" className={'links'}>Projects</NavLink>
-//             <NavLink to="/contact" className={'links'}>Contact</NavLink>
-//         </Nav>
-//     )
-// }
-
-
-// const styles = {
-//     marginBottom: 50
-//   };
-  
-//   const CustomNav = ({ active, onSelect, ...props }) => {
-//     return (
-//       <Nav {...props} activeKey={active} onSelect={onSelect} style={styles}>
-//         <Nav.Item eventKey="home" icon={<Icon icon="home" />}>
-//           Home
-//         </Nav.Item>
-//         {/* <Nav.Item eventKey="home">Home</Nav.Item> */}
-//         <Nav.Item eventKey="skills">Skills</Nav.Item>
-//         <Nav.Item eventKey="projects">Projects</Nav.Item>
-//         <Nav.Item eventKey="contact">Contact</Nav.Item>
-//       </Nav>
-//     );
-//   };
-  
-//   class Nav extends React.Component {
-//     constructor() {
-//       super();
-//       this.state = {
-//         active: 'home'
-//       };
-//       this.handleSelect = this.handleSelect.bind(this);
-//     }
-//     handleSelect(activeKey) {
-//       this.setState({ active: activeKey });
-//     }
-//     render() {
-//       const { active } = this.state;
-//       return (
-//         <div>
-//           <CustomNav active={active} onSelect={this.handleSelect} />
-//           <CustomNav appearance="tabs" active={active} onSelect={this.handleSelect} />
-//           <CustomNav appearance="tabs" reversed active={active} onSelect={this.handleSelect} />
-//           <CustomNav appearance="subtle" active={active} onSelect={this.handleSelect} />
-//           <CustomNav appearance="subtle" reversed active={active} onSelect={this.handleSelect} />
-//         </div>
-//       );
-//     }
-//   }
-  
-//  export default Nav;
