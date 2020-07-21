603a38a4f0aba67ca8842834ad843ecb12b525e8
Maggie price <maggieprice2016@gmail.com>
2020-07-21 19:22:50 -0400

    added skills to backend category

--- src/components/SkillsAPI.js
+++ src/components/SkillsAPI.js
@@ -23,7 +23,10 @@ export const SkillsAPI = [
     list: [
       {name: 'NodeJS', image:'xyz', level: "three"},
       {name: 'Express', image:'xyz', level: "three"},
-      {name: 'SQL', image:'xyz', level: "three"}
+      {name: 'SQL', image:'xyz', level: "three"},
+      {name: 'REST', image:'xyz', level: "three"},
+      {name: 'Knex', image:'xyz', level: "three"},
+      {name: 'Jest Testing', image:'xyz', level: "three"}
   ],
 },
 { 
@@ -96,107 +99,4 @@ category: 'Tech',
 //    },
 
 
-// export default SkillsAPI;
-
-// export const skillsFrontEnd = [
-// 	{
-// 		category: 'Fundamentals',
-// 		skills: [
-// 			{ icon: 'html5', skill: 'HTML5' },
-// 			{ icon: 'css3-alt', skill: 'CSS3' },
-// 			{ icon: 'js-square', skill: 'JavaScript 6' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Libraries/Frameworks',
-// 		skills: [
-// 			{ icon: 'react', skill: 'ReactJS (Hooks, Context API)' },
-// 			// { icon: 'react', skill: 'React Hooks' },
-// 			// { icon: 'react', skill: 'Context API' },
-// 			{ svg: 'redux', skill: 'Redux' },
-// 			{ svg: 'apollo', skill: 'Apollo Client' },
-// 			{ icon: 'vuejs', skill: 'Vue.js (Vue Router, Vuex)' },
-// 			// { icon: 'angular', skill: 'Angular' },
-// 		],
-// 	},
-// 	{
-// 		category: 'CSS Compilers',
-// 		skills: [
-// 			{ icon: 'less', skill: 'LESS' },
-// 			{ icon: 'sass', skill: 'SASS/SCSS' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Styling Libraries',
-// 		skills: [
-// 			{ skill: 'Styled-Components' },
-// 			{ skill: 'Semantic UI' },
-// 			{ skill: 'Material UI' },
-// 		],
-// 	},
-// 	{
-// 		category: 'API Calls',
-// 		skills: [
-// 			{ svg: 'axios', skill: 'Axios' },
-// 			{ icon: 'js-square', skill: 'Fetch API' },
-// 			{ svg: 'graphql', skill: 'GraphQL' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Deployment',
-// 		skills: [
-// 			{ svg: 'netlify', skill: 'Netlify' },
-// 			{ svg: 'zeit', skill: 'Zeit' },
-// 			// { icon: 'aws', skill: 'Elastic Beanstalk' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Version Control',
-// 		skills: [{ icon: 'git', skill: 'Git' }],
-// 	},
-// 	{
-// 		category: 'Testing',
-// 		skills: [
-// 			{ svg: 'jest', skill: 'Jest' },
-// 			{ skill: 'React-Testing-Library' },
-// 		],
-// 	},
-// ];
-
-// export const skillsBackEnd = [
-// 	{
-// 		category: 'Fundamentals',
-// 		skills: [
-// 			{ icon: 'node-js', skill: 'Node.js' },
-// 			{ icon: 'python', skill: 'Python' },
-// 			{ skill: 'REST' },
-// 			{ svg: 'sql', skill: 'SQL' },
-// 			{ svg: 'graphql', skill: 'GraphQL' },
-// 			{ icon: 'aws', skill: 'DynamoDB' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Frameworks',
-// 		skills: [
-// 			{ icon: 'node-js', skill: 'Express' },
-// 			{ svg: 'knex', skill: 'Knex' },
-// 			{ svg: 'graphql', skill: 'GraphQL-Yoga' },
-// 			{ svg: 'apollo', skill: 'Apollo Server (Gateway, Federation)' },
-// 			{ svg: 'prisma', skill: 'Prisma' },
-// 			// { svg: 'apollo', skill: 'Apollo Gateway' },
-// 			// { svg: 'apollo', skill: 'Apollo Federation' },
-// 			{ svg: 'django', skill: 'Django' },
-// 		],
-// 	},
-// 	{
-// 		category: 'Deployment',
-// 		skills: [{ svg: 'heroku', skill: 'Heroku' }],
-// 	},
-// 	{
-// 		category: 'Testing',
-// 		skills: [
-// 			{ svg: 'jest', skill: 'Jest' },
-// 			{ icon: 'node-js', skill: 'SuperTest' },
-// 		],
-// 	},
 ];
