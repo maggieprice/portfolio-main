88d496dd5ce309e14d9e240b348f92fd9c81cae8
Maggie price <maggieprice2016@gmail.com>
2020-07-28 11:27:39 -0400

    finished skills sectionn minus light stylig changes

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,10 @@ import {
   CardBody,
   CardHeader,
   CardFooter,
-  // CardFooter,
+  Alert 
+  // Modal, 
+  // ModalBody, 
+  // ModalHeader 
 } from "shards-react";
 
 const Skills = () => {
@@ -15,38 +18,102 @@ const Skills = () => {
     <div className="page">
       <div>
         <h3 className="skills-categories">Web Development Skills and Tech Stack </h3>
+          <Alert><h4>Honorable Mention: Design</h4>
+            <p> PhotoShop | InDesign | Invitations | Flyers | Advertisements | Basic Logos</p> </Alert> 
+          <Alert> <h4>Honorable Mention: Office Skills</h4>
+            <p> Banking/Bookkeeping | Correspondence, Dictation, Scheduling | Accounts Payable/Receiveable | Compliance, Records | Quicken/Quickbooks</p>
+                 </Alert>
         <div className='skills-categories'>
  								{SkillsAPI.map(cat => {
-                  //  var techClass = document.getElementsByName("Tech");
-                  //  techClass.className += " tech"
                    return (
+                    <div className="skills-container">
+                    <div className="skills-card" key={cat.id}>
+                    <Card className="card"> 
+                      <CardHeader className="card-title">{cat.type}</CardHeader>
+                      <CardTitle>{cat.list[0].name}</CardTitle>
+                      <CardImg className="card-img" src={cat.list[0].image}/>
+                      <CardBody className="card-body">{cat.list[0].level}</CardBody>
+                      <CardFooter>{cat.category}</CardFooter>
+                   </Card> 
+                 </div>
                    <div className="skills-card" key={cat.id}>
                      <Card className="card"> 
                        <CardHeader className="card-title">{cat.type}</CardHeader>
                        <CardTitle>{cat.list[1].name}</CardTitle>
                        <CardImg className="card-img" src={cat.list[1].image}/>
                        <CardBody className="card-body">{cat.list[1].level}</CardBody>
-                       <CardFooter>{cat.category}</CardFooter></Card> </div>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[2].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[2].image}/>
+                       <CardBody className="card-body">{cat.list[2].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[3].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[3].image}/>
+                       <CardBody className="card-body">{cat.list[3].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[4].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[4].image}/>
+                       <CardBody className="card-body">{cat.list[4].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[5].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[5].image}/>
+                       <CardBody className="card-body">{cat.list[5].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[6].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[6].image}/>
+                       <CardBody className="card-body">{cat.list[6].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[7].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[7].image}/>
+                       <CardBody className="card-body">{cat.list[7].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                  <div className="skills-card" key={cat.id}>
+                     <Card className="card"> 
+                       <CardHeader className="card-title">{cat.type}</CardHeader>
+                       <CardTitle>{cat.list[8].name}</CardTitle>
+                       <CardImg className="card-img" src={cat.list[8].image}/>
+                       <CardBody className="card-body">{cat.list[8].level}</CardBody>
+                       <CardFooter>{cat.category}</CardFooter>
+                    </Card> 
+                  </div>
+                 </div>
 								)})}
                 
-              
-							</div>
+              </div>
       </div>
-      <div className="design-skills">
-        <h3>Honorable Mention to things I enjoy creating as a designer:</h3>
-        <ul>
-          <li> Invitations</li>
-          <li> Flyers</li>
-          <li> Advertisements</li>
-          <li> Basic Logos</li>
-        </ul>
-        <h3>Tools:</h3>
-        <ul>
-          <li>PhotoShop</li>
-          <li>InDesign</li>
-          <li>Pen and Paper</li>
-        </ul>
-      </div>
+      
     </div>
   );
 };
--- src/components/SkillsAPI.js
+++ src/components/SkillsAPI.js
@@ -2,109 +2,49 @@ export const SkillsAPI = [
   { 
       id: 1,
       category: 'Tech',
-      type: 'Language',
+      type: 'Languages  Frameworks    Styling',
       list: [
         {name: 'Javascript', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png', level: "three"},
-        {name: 'HTML', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/html-5-512.pnghttps://cdn4.iconfinder.com/data/icons/logos-3/504/html5-512.png', level: "three"},
-        {name: 'CSS', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png', level: "three"}
+        {name: 'HTML', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/html-5-512.png', level: "three"},
+        {name: 'React / Redux', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png', level: "three"},
+        {name: 'Hooks/Context API', image:'https://cdn4.iconfinder.com/data/icons/web-development-5/500/api-code-window-512.png', level: "three"},
+        {name: 'CSS', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png', level: "three"},
+        {name: 'Less', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/less-512.png', level: "three"},
+        {name: 'Bootstrap', image:'https://cdn4.iconfinder.com/data/icons/blackicon/54/bootstrap_icon-256.png', level: "three"},
+        {name: 'Styled Components / Material UI', image:'https://styled-components.com/logo.png', level: "three"},
+        {name: 'Sass', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/sass-256.png', level: "three"}
     ],
 },
 { 
   id: 2,  
   category: 'Tech',
-    type: 'Frameworks',
-    list: [
-      {name: 'React', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png', level: "three"},
-      {name: 'Redux', image:'xyz', level: "three"},
-      {name: 'Hooks/Context API', image:'xyz', level: "three"}
-  ],
-},
-{ 
-  id: 3,  
-  category: 'Tech',
     type: 'Backend',
     list: [
       {name: 'NodeJS', image:'https://cdn0.iconfinder.com/data/icons/programming-vintage-circle/32/programming_nodejs_icon-512.png', level: "three"},
-      {name: 'Express', image:'xyz', level: "three"},
-      {name: 'SQL', image:'xyz', level: "three"},
-      {name: 'REST', image:'xyz', level: "three"},
-      {name: 'Knex', image:'xyz', level: "three"},
-      {name: 'Jest Testing', image:'xyz', level: "three"}
+      {name: 'Express', image:'https://cdn2.iconfinder.com/data/icons/amazon-aws-stencils/100/SDKs_copy_nodeJS-256.png', level: "three"},
+      {name: 'SQL', image:'https://cdn3.iconfinder.com/data/icons/basicolor-computer-tech/24/091_database_data_sql-256.png', level: "three"},
+      {name: 'REST', image:'https://cdn2.iconfinder.com/data/icons/picol-vector/32/api-256.png', level: "three"},
+      {name: 'Knex', image:'http://knexjs.org/assets/images/knex.png', level: "three"},
+      {name: 'Jest Testing', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/jest-256.png', level: "three"},
+      {name: 'Postman', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/postman-256.png', level: "three"},
+      {name: 'Sqlite3', image:'https://cdn3.iconfinder.com/data/icons/gis/layer-sqlite-add.png', level: "three"},
+      {name: 'heroku', image:'https://cdn1.iconfinder.com/data/icons/simple-icons/256/heroku-256-black.png', level: "three"}
   ],
 },
 { 
-  id: 4,  
+  id: 3,   
   category: 'Tech',
-    type: 'styling',
-    list: [
-      {name: 'CSS', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png', level: "three"},
-      {name: 'Less', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/less-512.png', level: "three"},
-      {name: 'Sass', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/sass-512.png', level: "three"},
-      {name: 'Styled Components', image:'xyz', level: "three"},
-      {name: 'Material UI', image:'xyz', level: "three"},
-      {name: 'Sass', image:'xyz', level: "three"}
-  ],
-},
-{ 
-  id: 5,   
-  category: 'Tech',
-    type: 'cs',
+    type: 'CS & Tools',
     list: [
       {name: 'Python', image:'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png', level: "three"},
-      {name: 'BigO', image:'xyz', level: "three"},
-      {name: 'Data Structures', image:'xyz', level: "three"},
-      {name: 'Algorithims', image:'xyz', level: "three"}
-  ],
-},
-{
-  id: 6,
-    category: 'Tech',
-    type: 'deployment',
-    list: [
+      {name: 'BigO', image:'https://cdn1.iconfinder.com/data/icons/big-data-orchid-vol-1/256/Algorithm-256.png', level: "three"},
+      {name: 'Data Structures', image:'https://cdn4.iconfinder.com/data/icons/essential-app-1/16/cluster-data-group-organize-256.png', level: "three"},
+      {name: 'Algorithims', image:'https://cdn2.iconfinder.com/data/icons/analytic-investment-and-balanced-scorecard/512/644_Algorithm_brain_conclusion_process_thinking-256.png', level: "three"},
+      {name: 'Figma', image:'https://cdn4.iconfinder.com/data/icons/bloomies-webdesign-tools/25/Figma_square-256.png', level: "three"},
+      {name: 'Trello', image:'https://cdn1.iconfinder.com/data/icons/designer-skills/128/trello-512.png', level: "three"},
+      {name: 'VSCode, Codepen, CodeSandbox', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/visualstudio-microsoft-webdesign-html-css-javascript-develop-512.png', level: "three"},
       {name: 'netlify', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/netlify-512.png', level: "three"},
-      {name: 'vercel', image:'xyz', level: "three"},
-      {name: 'heroku', image:'xyz', level: "three"}
+      {name: 'vercel', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/zeit-128.png', level: "three"}
   ],
 },
-{
-        id: 7,
-        category: 'Tech',
-        type: 'tools',
-        list: [
-          {name: 'Github', image:'https://cdn1.iconfinder.com/data/icons/free-social-media-14/32/github_social_media_connect-256.png', level: "three"},
-          {name: 'Git', image:'xyz', level: "three"},
-          {name: 'Postman', image:'xyz', level: "three"},
-          {name: 'Sqlite3', image:'xyz', level: "three"},
-          {name: 'Figma', image:'xyz', level: "three"},
-          {name: 'Trello', image:'https://cdn1.iconfinder.com/data/icons/designer-skills/128/trello-512.png', level: "three"},
-          {name: 'Codepen', image:'xyz', level: "three"},
-          {name: 'CodeSandbox', image:'xyz', level: "three"},
-          {name: 'VSCode', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/visualstudio-microsoft-webdesign-html-css-javascript-develop-512.png', level: "three"},
-          {name: 'Terminal', image:'xyz', level: "three"}
-      ],
-    },
-    {
-            id: 8,  
-            category: 'Office/Soft',
-            type: 'all',
-            list: [
-              {name: 'Banking', image:'https://cdn0.iconfinder.com/data/icons/elasto-online-store/26/00-ELASTOFONT-STORE-READY_bank-512.png', level: "three"},
-              {name: 'Bookeeping', image:'xyz', level: "three"},
-              {name: 'Accounts Payable/Receivable', image:'xyz', level: "three"},
-              {name: 'Correspondence', image:'xyz', level: "three"},
-              {name: 'Dictation', image:'xyz', level: "three"},
-              {name: 'Scheduling', image:'xyz', level: "three"},
-              {name: 'Compliance', image:'xyz', level: "three"},
-              {name: 'ReccordsKeeping', image:'xyz', level: "three"},
-              {name: 'Office Equipment', image:'xyz', level: "three"},
-              {name: 'Microsoft Office', image:'xyz', level: "three"},
-              {name: 'Quicken/Quickbooks', image:'xyz', level: "three"}
-          ],
-        }
-
-//  Libraries : {
-//      Bootstrap : { name: 'Bootstrap', image: 'https://cdn4.iconfinder.com/data/icons/logos-3/504/Bootstrap-512.png , level: "three" }, 
-//    },
-
-
 ];
