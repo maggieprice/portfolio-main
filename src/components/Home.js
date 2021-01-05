da0d825d68271ae1ed54b3eda43c7d381b46af7e
Justin Price <mr.price@Justins-iMac.local>
2021-01-04 23:06:27 -0500

    some color changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,63 +4,69 @@ import "../name.css";
 function Home() {
   return (
     <div className="page">
-      <div className="top">
-        <div>
-          <img
-            className="header-img"
-            src="https://images.unsplash.com/photo-1503891450247-ee5f8ec46dc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
-            alt="miami"
-          />
-          <h3 className="coral">Looking to hire a web developer? Meet me.</h3>
-          {/* <h1 className="name">{"<"} Maggie J. Price {"/>"}</h1> */}
-          {/* <div class="wrapper">
+      <section className="content">
+        <div className="top">
+          <div>
+            <img
+              className="header-img"
+              src="https://images.unsplash.com/photo-1503891450247-ee5f8ec46dc3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
+              alt="miami"
+            />
+            <h3 className="coral">Looking to hire a web developer? Meet me.</h3>
+            {/* <h1 className="name">{"<"} Maggie J. Price {"/>"}</h1> */}
+            {/* <div class="wrapper">
             <div class="neon-wrapper"> */}
-          <span class="txt">
-            {"<"} Maggie J. Price {"/>"}
-          </span>
-          <span class="gradient"></span>
-          <span class="dodge"></span>
-          {/* </div>
+            <span class="txt">
+              {"<"} Maggie J. Price {"/>"}
+            </span>
+            <span class="gradient"></span>
+            <span class="dodge"></span>
+            {/* </div>
           </div> */}
-          <h3 className="coral">Full Stack Web Developer/Software Engineer</h3>
-        </div>
-      </div>
-      <div className="about">
-        {/* <img > */}
-        <div className="about-paragraph">
-          <h3>Who am I?</h3>
-          <p className="content-left">
-            Hi. I am a creative. Realizing this allowed me to find my passion in
-            web development. I love creating and problem solving. So, web
-            development is very fulfilling for me. I am also pursuing a Bachelor
-            of Business Economics degree because... to be honest, I love
-            learning. Learning is my happy place. Learning is my down time. This
-            is just another reason why I love development! There is no shortage
-            of learning opportunities.{" "}
-          </p>
-          <div className="hobbies">
-            <h3>What do I Love?</h3>
-            <ul className="content-right">
-              <p>Traveling</p>
-              <p>Decorating</p>
-              <p>Singing</p>
-              <p>Playing Piano</p>
-              <p>Listening to Learn (Podcasts/Youtube)</p>
-              <p>Streaming TV</p>
-              <p>Listening to Music</p>
-              <p>Helping Others</p>
-              <p>Crafting</p>
-            </ul>
+            <h3 className="coral">
+              Full Stack Web Developer/Software Engineer
+            </h3>
           </div>
-          <h3>What is My Personal Mission?</h3>
-          <p className="content-left">
-            My personal mission is to disrupt the current tech space with
-            innovative technologies. I intend to learn and grow consistently to
-            accomplish this. I am really facinated by the role that technology
-            plays in "changing the world" and I want to participate.
-          </p>
         </div>
-      </div>
+        <div className="about">
+          {/* <img > */}
+          <div className="about-paragraph">
+            <h3>Who am I?</h3>
+            <p className="content-left">
+              Hi. I am a creative. Realizing this allowed me to find my passion
+              in web development. I love creating and problem solving. So, web
+              development is very fulfilling for me. I am also pursuing a
+              Bachelor of Business Economics degree because... to be honest, I
+              love learning. Learning is my happy place. Learning is my down
+              time. This is just another reason why I love development! There is
+              no shortage of learning opportunities.{" "}
+            </p>
+            <div className="hobbies">
+              <h3>What do I Love?</h3>
+              <ul className="content-right">
+                <p>Traveling</p>
+                <p>Decorating</p>
+                <p>Singing</p>
+                <p>Playing Piano</p>
+                <p>Listening to Learn (Podcasts/Youtube)</p>
+                <p>Streaming TV</p>
+                <p>Listening to Music</p>
+                <p>Helping Others</p>
+                <p>Crafting</p>
+                <p>Making Music</p>
+              </ul>
+            </div>
+            <h3>What is My Personal Mission?</h3>
+            <p className="content-left">
+              My personal mission is to disrupt the current tech space with
+              innovative technologies. I intend to learn and grow consistently
+              to accomplish this. I am really facinated by the role that
+              technology plays in "changing the world" and I want to
+              participate.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
