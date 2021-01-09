58b2deced04f60aab051507730bb83dfbdf48d7a
Justin Price <mr.price@Justins-iMac.local>
2021-01-09 00:07:57 -0500

    made cards better for skills, deleted unnecessary info

--- src/components/SkillsAPI.js
+++ src/components/SkillsAPI.js
@@ -1,50 +1,153 @@
 export const SkillsAPI = [
-  { 
-      id: 1,
-      category: 'Tech',
-      type: 'Languages  Frameworks    Styling',
-      list: [
-        {name: 'Javascript', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png', level: "three"},
-        {name: 'HTML', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/html-5-512.png', level: "three"},
-        {name: 'React / Redux', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png', level: "three"},
-        {name: 'Hooks/Context API', image:'https://cdn4.iconfinder.com/data/icons/web-development-5/500/api-code-window-512.png', level: "three"},
-        {name: 'CSS', image:'https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png', level: "three"},
-        {name: 'Less', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/less-512.png', level: "three"},
-        {name: 'Bootstrap', image:'https://cdn4.iconfinder.com/data/icons/blackicon/54/bootstrap_icon-256.png', level: "three"},
-        {name: 'Styled Components / Material UI', image:'https://styled-components.com/logo.png', level: "three"},
-        {name: 'Sass', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/sass-256.png', level: "three"}
+  {
+    id: 1,
+    type: "Languages  Frameworks    Styling",
+    list: [
+      {
+        name: "Javascript",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/designer-skills/128/code-programming-javascript-software-develop-command-language-512.png",
+      },
+      {
+        name: "HTML",
+        image:
+          "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/html-5-512.png",
+      },
+      {
+        name: "React / Redux",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/designer-skills/128/react-512.png",
+      },
+      {
+        name: "Hooks/Context API",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/web-development-5/500/api-code-window-512.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://cdn1.iconfinder.com/data/icons/social-media-logos-7/64/css-3-512.png",
+      },
+      {
+        name: "Less",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/designer-skills/128/less-512.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/blackicon/54/bootstrap_icon-256.png",
+      },
+      {
+        name: "Styled Components / Material UI",
+        image: "https://styled-components.com/logo.png",
+      },
+      {
+        name: "Sass",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/designer-skills/128/sass-256.png",
+      },
     ],
-},
-{ 
-  id: 2,  
-  category: 'Tech',
-    type: 'Backend',
+  },
+  {
+    id: 2,
+    type: "Backend",
     list: [
-      {name: 'NodeJS', image:'https://cdn0.iconfinder.com/data/icons/programming-vintage-circle/32/programming_nodejs_icon-512.png', level: "three"},
-      {name: 'Express', image:'https://cdn2.iconfinder.com/data/icons/amazon-aws-stencils/100/SDKs_copy_nodeJS-256.png', level: "three"},
-      {name: 'SQL', image:'https://cdn3.iconfinder.com/data/icons/basicolor-computer-tech/24/091_database_data_sql-256.png', level: "three"},
-      {name: 'REST', image:'https://cdn2.iconfinder.com/data/icons/picol-vector/32/api-256.png', level: "three"},
-      {name: 'Knex', image:'http://knexjs.org/assets/images/knex.png', level: "three"},
-      {name: 'Jest Testing', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/jest-256.png', level: "three"},
-      {name: 'Postman', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/postman-256.png', level: "three"},
-      {name: 'Sqlite3', image:'https://cdn3.iconfinder.com/data/icons/gis/layer-sqlite-add.png', level: "three"},
-      {name: 'heroku', image:'https://cdn1.iconfinder.com/data/icons/simple-icons/256/heroku-256-black.png', level: "three"}
-  ],
-},
-{ 
-  id: 3,   
-  category: 'Tech',
-    type: 'CS & Tools',
+      {
+        name: "NodeJS",
+        image:
+          "https://cdn0.iconfinder.com/data/icons/programming-vintage-circle/32/programming_nodejs_icon-512.png",
+      },
+      {
+        name: "Express",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/amazon-aws-stencils/100/SDKs_copy_nodeJS-256.png",
+      },
+      {
+        name: "SQL",
+        image:
+          "https://cdn3.iconfinder.com/data/icons/basicolor-computer-tech/24/091_database_data_sql-256.png",
+      },
+      {
+        name: "REST",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/picol-vector/32/api-256.png",
+      },
+      {
+        name: "Knex",
+        image: "http://knexjs.org/assets/images/knex.png",
+      },
+      {
+        name: "Jest Testing",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/jest-256.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/postman-256.png",
+      },
+      {
+        name: "Sqlite3",
+        image:
+          "https://cdn3.iconfinder.com/data/icons/gis/layer-sqlite-add.png",
+      },
+      {
+        name: "heroku",
+        image:
+          "https://cdn1.iconfinder.com/data/icons/simple-icons/256/heroku-256-black.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    type: "CS & Tools",
     list: [
-      {name: 'Python', image:'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png', level: "three"},
-      {name: 'BigO', image:'https://cdn1.iconfinder.com/data/icons/big-data-orchid-vol-1/256/Algorithm-256.png', level: "three"},
-      {name: 'Data Structures', image:'https://cdn4.iconfinder.com/data/icons/essential-app-1/16/cluster-data-group-organize-256.png', level: "three"},
-      {name: 'Algorithims', image:'https://cdn2.iconfinder.com/data/icons/analytic-investment-and-balanced-scorecard/512/644_Algorithm_brain_conclusion_process_thinking-256.png', level: "three"},
-      {name: 'Figma', image:'https://cdn4.iconfinder.com/data/icons/bloomies-webdesign-tools/25/Figma_square-256.png', level: "three"},
-      {name: 'Trello', image:'https://cdn1.iconfinder.com/data/icons/designer-skills/128/trello-512.png', level: "three"},
-      {name: 'VSCode, Codepen, CodeSandbox', image:'https://cdn2.iconfinder.com/data/icons/designer-skills/128/visualstudio-microsoft-webdesign-html-css-javascript-develop-512.png', level: "three"},
-      {name: 'netlify', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/netlify-512.png', level: "three"},
-      {name: 'vercel', image:'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/zeit-128.png', level: "three"}
-  ],
-},
+      {
+        name: "Python",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/267_Python_logo-512.png",
+      },
+      {
+        name: "BigO",
+        image:
+          "https://cdn1.iconfinder.com/data/icons/big-data-orchid-vol-1/256/Algorithm-256.png",
+      },
+      {
+        name: "Data Structures",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/essential-app-1/16/cluster-data-group-organize-256.png",
+      },
+      {
+        name: "Algorithims",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/analytic-investment-and-balanced-scorecard/512/644_Algorithm_brain_conclusion_process_thinking-256.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/bloomies-webdesign-tools/25/Figma_square-256.png",
+      },
+      {
+        name: "Trello",
+        image:
+          "https://cdn1.iconfinder.com/data/icons/designer-skills/128/trello-512.png",
+      },
+      {
+        name: "VSCode, Codepen, CodeSandbox",
+        image:
+          "https://cdn2.iconfinder.com/data/icons/designer-skills/128/visualstudio-microsoft-webdesign-html-css-javascript-develop-512.png",
+      },
+      {
+        name: "netlify",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/netlify-512.png",
+      },
+      {
+        name: "vercel",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/zeit-128.png",
+      },
+    ],
+  },
 ];
