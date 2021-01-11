6c049124e214a3f822fdb761e40a13c188bc399c
Justin Price <mr.price@Justins-iMac.local>
2021-01-10 22:25:12 -0500

    contact form works

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,100 +1,72 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 
-class ContactForm extends Component {
-  handleSubmit(e) {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const subject = document.getElementById("subject").value;
-    const message = document.getElementById("message").value;
-    axios({
-      method: "POST",
-      url: "https://git.heroku.com/portfoliomail.git",
-      data: {
-        name: name,
-        email: email,
-        subject: subject,
-        messsage: message,
-      },
-    }).then((response) => {
-      if (response.data.msg === "success") {
-        alert("Message Sent.");
-        this.resetForm();
-      } else if (response.data.msg === "fail") {
-        alert("Message failed to send.");
-      }
-    });
-    
-  }
-
-  resetForm() {
-    document.getElementById("contact-form").reset();
-  }
-
-  render() {
-    return (
-      <div className="page">
-        <div className="contact-head">
-          <h1>We Should Hang Out...</h1>
-        </div>
-        <div className="contact-logos">
-          <a href="https://github.com/maggieprice">
-            <img
-              className="contact-logos"
-              src="https://cdn1.iconfinder.com/data/icons/free-social-media-14/32/github_social_media_connect-256.png"
-              alt="Github Logo that Links to My Profile"
-            />
-          </a>
-          <a href="https://www.linkedin.com/in/maggieprice1/">
-            <img
-              className="contact-logos"
-              src="https://cdn1.iconfinder.com/data/icons/social-media-free-26/32/linkedin_social_media_connect-256.png"
-              alt="LinkedIn Logo that Links to My Profile"
-            />
-          </a>
-        </div>
-
-        <div className="col-sm-4 offset-sm-4">
-          <form
-            id="contact-form"
-            onSubmit={this.handleSubmit.bind(this)}
-            method="POST"
-          >
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="subject">Subject</label>
-              <input
-                type="text"
-                className="form-control"
-                id="subject"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <input className="form-control" rows="5" id="message"></input>
-            </div>
-            <button onSubmit={this.handleSubmit} className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
+function Contact() {
+  return (
+    <div className="page">
+      <div className="contact-head">
+        <h1>We Should Hang Out...</h1>
       </div>
-    );
-  }
+      <div className="contact-logos">
+        <a href="https://github.com/maggieprice">
+          <img
+            className="contact-logos"
+            src="https://cdn1.iconfinder.com/data/icons/free-social-media-14/32/github_social_media_connect-256.png"
+            alt="Github Logo that Links to My Profile"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/maggieprice1/">
+          <img
+            className="contact-logos"
+            src="https://cdn1.iconfinder.com/data/icons/social-media-free-26/32/linkedin_social_media_connect-256.png"
+            alt="LinkedIn Logo that Links to My Profile"
+          />
+        </a>
+      </div>
+      <div className="col-sm-4 offset-sm-4">
+        <form action="https://formspree.io/f/mjvpjakj" method="POST">
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input type="text" className="form-control" name="name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              aria-describedby="emailHelp"
+              name="email"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="subject">Subject</label>
+            <input
+              type="text"
+              className="form-control"
+              aria-describedby="emailHelp"
+              name="subject"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message</label>
+            <input className="form-control" rows="5" name="message"></input>
+          </div>
+          {/* <label>
+            Your email:
+            <input type="text" name="_replyto" />
+          </label>
+          <label>
+            Your message:
+            <textarea name="message"></textarea>
+          </label> */}
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+{
+  /* } */
 }
 
-export default ContactForm;
+export default Contact;
