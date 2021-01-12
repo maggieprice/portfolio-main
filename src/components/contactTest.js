41b8de24d2a9b2ec888072421069cd9e38bafa50
Justin Price <mr.price@Justins-iMac.local>
2021-01-11 21:12:43 -0500

    typo

---
+++ src/components/contactTest.js
@@ -0,0 +1,94 @@
+{
+  /* <form
+  action="https://formspree.io/f/mjvpjakj"
+  method="POST"
+>
+  <label>
+    Your email:
+    <input type="text" name="_replyto">
+  </label>
+  <label>
+    Your message:
+    <textarea name="message"></textarea>
+  </label> */
+}
+
+{
+  /* <!-- your other form fields go here --> */
+}
+
+{
+  /* <button type="submit">Send</button>
+</form> */
+}
+
+// / class ContactForm extends Component {
+//   handleSubmit(e) {
+//     e.preventDefault();
+//     const name = document.getElementById("name").value;
+//     const email = document.getElementById("email").value;
+//     const subject = document.getElementById("subject").value;
+//     const message = document.getElementById("message").value;
+//     axios({
+//       method: "POST",
+//       url: "https://git.heroku.com/portfoliomail.git",
+//       data: {
+//         name: name,
+//         email: email,
+//         subject: subject,
+//         messsage: message,
+//       },
+//     }).then((response) => {
+//       if (response.data.msg === "success") {
+//         alert("Message Sent.");
+//         this.resetForm();
+//       } else if (response.data.msg === "fail") {
+//         alert("Message failed to send.");
+//       }
+//     });
+
+//   }
+
+//   resetForm() {
+//     document.getElementById("contact-form").reset();
+//   }
+
+//   render() {
+
+{
+  /* <form
+            id="contact-form"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="subject">Subject</label>
+              <input
+                type="text"
+                className="form-control"
+                id="subject"
+                aria-describedby="emailHelp"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="message">Message</label>
+              <input className="form-control" rows="5" id="message"></input>
+            </div>
+            <button onSubmit={this.handleSubmit} className="btn btn-primary">
+              Submit
+            </button>
+          </form> */
+}
